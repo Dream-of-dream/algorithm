45c0b32480c67512cd88ca8e8f18d30ce365b55f
Yong-Bin-Park <dydqls7592@gmail.com>
2022-05-06 10:53:14 +0900

    요소에 맞는 speed 수정

--- algorithm_Sort/script/animation.js
+++ algorithm_Sort/script/animation.js
@@ -19,6 +19,8 @@ function animate(solution) {
     const frames = solution.getFrames();
     for (let i = 0; i < frames.length; ++i) {
         (function(frames, i, bars, ANIMATION_FRAMES, SPEED, TOTAL_ELEMENTS) {
+            //요소 개수에 따라서 속도 달라지는 부분
+            if(TOTAL_ELEMENTS <= 15){
             ANIMATION_FRAMES.push(
                 setTimeout(function() {
                     $(".bar").removeClass("compared");
@@ -46,7 +48,66 @@ function animate(solution) {
                     }
                 }, SPEED * TOTAL_ELEMENTS * i)
             );
-        })(frames, i, bars, ANIMATION_FRAMES, SPEED, TOTAL_ELEMENTS);
+            }
+            else if(TOTAL_ELEMENTS <= 25){
+                ANIMATION_FRAMES.push(
+                    setTimeout(function() {
+                        $(".bar").removeClass("compared");
+                        const lastFrame = i == frames.length - 1;
+                        const elem = frames[i].elements;
+                        const highlight = frames[i].highlights;
+    
+                        // Highlight compared elements
+                        if (highlight.length) {
+                            for (h = 0; h < highlight.length; ++h) {
+                                $(bars[highlight[h]]).addClass("compared");
+                            }
+                        }
+    
+                        // Swap compared elements
+                        if (elem.length) {
+                            $(bars[elem[1]]).swap(bars[elem[0]]);
+                        }
+    
+                        // Disable stop button once animation ends
+                        if (lastFrame) {
+                            $("#stop")
+                                .attr("disabled", true)
+                                .removeClass("green");
+                        }
+                    }, (SPEED*TOTAL_ELEMENTS/80) * TOTAL_ELEMENTS * i)
+                );
+            }
+            else {
+                ANIMATION_FRAMES.push(
+                    setTimeout(function() {
+                        $(".bar").removeClass("compared");
+                        const lastFrame = i == frames.length - 1;
+                        const elem = frames[i].elements;
+                        const highlight = frames[i].highlights;
+    
+                        // Highlight compared elements
+                        if (highlight.length) {
+                            for (h = 0; h < highlight.length; ++h) {
+                                $(bars[highlight[h]]).addClass("compared");
+                            }
+                        }
+    
+                        // Swap compared elements
+                        if (elem.length) {
+                            $(bars[elem[1]]).swap(bars[elem[0]]);
+                        }
+    
+                        // Disable stop button once animation ends
+                        if (lastFrame) {
+                            $("#stop")
+                                .attr("disabled", true)
+                                .removeClass("green");
+                        }
+                    }, (SPEED*TOTAL_ELEMENTS/150) * TOTAL_ELEMENTS * i)
+                );
+            }
+        })(frames, i, bars, ANIMATION_FRAMES, SPEED , TOTAL_ELEMENTS);
     }
 }
 
